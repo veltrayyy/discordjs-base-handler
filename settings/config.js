7f9f4622b486b30c765cc434baab0239eeff81de
Vel <106902296+veltrayyy@users.noreply.github.com>
2022-06-17 20:07:38 +0700

    Initial commit

---
+++ settings/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  token:
+    process.env.TOKEN ||
+    "BOT_TOKEN",
+  prefix: process.env.PREFIX || "BOT_PREFIX",
+  embed: {
+    color: "#2f3136",
+    wrongcolor: "#2f3136",
+    footertext: "Coded By Kabir Singh | Tech Boy Development",
+    footericon:
+      "https://images-ext-1.discordapp.net/external/C2xH2P5W4KM4F6wPxElhlzyVrZ5bjWl9_TPT-X_3hfs/%3Fsize%3D512/https/cdn.discordapp.com/avatars/894158815166095390/b3963f5de220e55d185c5b9d15648a82.webp?width=424&height=424",
+  },
+  emoji: {
+    ERROR: "❌",
+    SUCCESS: "✅",
+    disabled: "🔴",
+    enabled: "🟢",
+    cleared: "🧹",
+    time: "⏲️",
+    search: "🔎",
+    ping: "🏓",
+    bot: "🤖",
+  },
+  // others
+  guildID : "" // for slash command
+};
